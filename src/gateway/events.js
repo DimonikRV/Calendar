d457a30d1a5d4b3a6f6308bf55ee00a112d0853f
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-03-20 13:27:51 +0200

    add

--- src/gateway/events.js
+++ src/gateway/events.js
@@ -1,41 +1,36 @@
-const webServerLink =
-  "https://63a6da56f8f3f6d4ab138405.mockapi.io/api/v1/events";
+const webServerLink = 'https://63a6da56f8f3f6d4ab138405.mockapi.io/api/v1/events';
 
 export const getEvents = async () => {
   const response = await fetch(webServerLink);
   if (!response.ok) {
-    throw new Error("Failed to load data");
+    throw new Error('Failed to load data');
   }
   const eventsData = await response.json();
   return eventsData;
 };
 
-export const postEvent = async (newEvent) => {
+export const postEvent = async newEvent => {
   const response = await fetch(webServerLink, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(newEvent),
   });
   if (!response.ok) {
-    throw new Error("Failed to load data");
+    throw new Error('Failed to load data');
   }
 };
 
-export const deleteEvent = async (eventId) => {
+export const deleteEvent = async eventId => {
   const response = await fetch(`${webServerLink}/${eventId}`, {
-    method: "DELETE",
+    method: 'DELETE',
   });
   if (!response.ok) {
-    throw new Error("Failed to delete event");
+    throw new Error('Failed to delete event');
   }
 };
-export const renderEvents = async (setEvents) => {
-  try {
-    const eventsData = await getEvents();
-    setEvents(eventsData);
-  } catch (error) {
-    alert("Can't display events");
-  }
+export const renderEvents = async setEvents => {
+  const eventsData = await getEvents();
+  setEvents(eventsData);
 };
